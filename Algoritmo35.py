b4cb56d65b17bc7474aae7164d0a4c117ddc2cc9
luisfe <luisfernando_contreras@hotmail.com>
2018-10-10 00:54:14 -0500

    first commit

---
+++ Algoritmo35.py
@@ -0,0 +1,11 @@
+"""Haga un diagrama de flujo que calcule la suma de los numeros pares comprendidos
+entre 10 y 50"""
+
+x = 0
+for l in range(10,50):
+    if l % 2 == 0:
+        x = x + l
+        print (x)
+    else:
+        pass
+print (x)
